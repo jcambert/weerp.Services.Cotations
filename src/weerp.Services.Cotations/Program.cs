ff9ed2fd2bff4acf15ba16af631515abcf65e533
Ambert Jean-Christophe <jc.ambert@free.fr>
2019-11-02 11:22:52 +0100

    First Commit

--- src/weerp.Services.Cotations/Program.cs
+++ src/weerp.Services.Cotations/Program.cs
@@ -1,11 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Autofac.Extensions.DependencyInjection;
+using MicroS_Common.Logging;
+using MicroS_Common.Metrics;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace weerp.Services.Cotations
 {
@@ -18,9 +15,15 @@ namespace weerp.Services.Cotations
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+             .UseServiceProviderFactory(new AutofacServiceProviderFactory())
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder
+                    .UseStartup<Startup>()
+                    .UseLogging()
+                    //.UseVault()
+                    //.UseLockbox()
+                    .UseAppMetrics();
                 });
     }
 }
--- src/weerp.Services.Cotations/Startup.cs
+++ src/weerp.Services.Cotations/Startup.cs
@@ -1,7 +1,20 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using Autofac;
+using AutoMapper;
+using Consul;
+using MicroS_Common;
+using MicroS_Common.Consul;
+using MicroS_Common.Dispatchers;
+using MicroS_Common.Jeager;
+using MicroS_Common.Mongo;
+using MicroS_Common.Mvc;
+using MicroS_Common.RabbitMq;
+using MicroS_Common.Redis;
+using MicroS_Common.Swagger;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,42 +23,64 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using weerp.domain;
 
 namespace weerp.Services.Cotations
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddCustomMvc();
+            services.AddSwaggerDocs();
+            services.AddConsul();
+            services.AddJaeger();
+            //services.AddOpenTracing();
+            services.AddRedis();
+            services.AddInitializers(typeof(IMongoDbInitializer));
+            services.AddAutoMapper(Assembly.GetEntryAssembly(), typeof(DomainProfile).Assembly);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(Assembly.GetEntryAssembly()).AsImplementedInterfaces();
+            builder.RegisterAssemblyTypes(typeof(DomainProfile).Assembly).AsImplementedInterfaces();
+            builder.AddRabbitMq();
+            builder.AddMongo();
+           // builder.AddMongoRepository<Product>("Products");
+            builder.AddDispatchers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IHostApplicationLifetime applicationLifetime, IConsulClient client,
+            IStartupInitializer startupInitializer)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            app.UseAllForwardedHeaders();
+            app.UseSwaggerDocs();
+            app.UseErrorHandler();
+            app.UseServiceId();
+#pragma warning disable MVC1005
+            app.UseMvc();
+#pragma warning restore MVC1005
+            app.UseRabbitMq()
+                /*.SubscribeCommand<CreateProduct>(onError: (c, e) => new CreateProductRejected(c.Id, e.Message, e.Code))
+                .SubscribeCommand<UpdateProduct>(onError: (c, e) => new UpdateProductRejected(c.Id, e.Message, e.Code))
+                .SubscribeCommand<DeleteProduct>(onError: (c, e) => new DeleteProductRejected(c.Id, e.Message, e.Code))
+                .SubscribeCommand<ReserveProducts>(onError: (c, e) => new ReserveProductsRejected(c.OrderId, e.Message, e.Code))
+                .SubscribeCommand<ReleaseProducts>(onError: (c, e) => new ReleaseProductsRejected(c.OrderId, e.Message, e.Code))*/;
 
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
+            var consulServiceId = app.UseConsul();
+            applicationLifetime.ApplicationStopped.Register(() =>
             {
-                endpoints.MapControllers();
+                client.Agent.ServiceDeregister(consulServiceId);
             });
+
+            startupInitializer.InitializeAsync();
         }
     }
 }
